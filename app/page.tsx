f47bda3f40b10027ca34c5737008936022841e62
Axadjon Mannonov <axadjonmannonov@MacBook-Pro--Axadjon.local>
2024-09-14 23:14:07 +0500

    Conflicts resolved

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,7 @@ import MemberIcon from '@rsuite/icons/Member';
 export default function Home() {
   return (
       <main>
-
-<h1 className='text-5xl text-gray-500 font-bold text-center animate-bounce mt-5'>Mannonov Axadjon
-</h1>
+<h1 className='text-5xl text-gray-500 font-bold text-center animate-bounce mt-5'>Mannonov Axadjon</h1>
       </main>
   );
 }
