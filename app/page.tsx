35f53d102e1976fba277d839d90a6a98d4f81b6e
Axadjon Mannonov <axadjonmannonov@MacBook-Pro--Axadjon.local>
2024-09-14 23:10:19 +0500

    mannonov

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+'use client';
+import MemberIcon from '@rsuite/icons/Member';
+
+
+
 
 
 export default function Home() {
   return (
       <main>
-       
+
+<h1 className='text-5xl text-gray-500 font-bold text-center animate-bounce mt-5'>Mannonov Axadjon
+</h1>
       </main>
   );
 }
